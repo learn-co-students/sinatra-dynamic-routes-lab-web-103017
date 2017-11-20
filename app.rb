8967f29b9c36f4d4f5236be1cbd8bd8254594f49
Josh-Stillman <joshstillman@gmail.com>
2017-11-20 17:37:25 -0500

    done

--- app.rb
+++ app.rb
@@ -2,5 +2,49 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
+
+  get '/square/:number' do
+    @n1 = params[:number].to_i
+    "#{@n1**2}"
+  end
+
+  get '/say/:number/:phrase' do
+    @format = params[:phrase].gsub('%20', ' ')
+    @num = params[:number].to_i
+
+    return_string = ""
+    @num.times do
+
+    return_string << @format + ' '
+  end
+
+  return_string
+
+
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    case params[:operation]
+    when "add"
+      "#{params[:number1].to_i + params[:number2].to_i}"
+    when "subtract"
+      "#{params[:number1].to_i - params[:number2].to_i}"
+    when "multiply"
+      "#{params[:number1].to_i * params[:number2].to_i}"
+    when "divide"
+      "#{params[:number1].to_i / params[:number2].to_i}"
+    else
+      "invalid operation"
+    end
+  end
+
 
 end
