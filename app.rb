ff6109be13b349815d4a247494ac8d9c7ad2b045
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2017-11-20 17:37:14 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,51 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name]
+    @name.reverse
+  end
+
+  get '/square/:number' do
+    @number = params[:number].to_i
+    "#{@number**2}"
+  end
+
+  get '/say/:number/:phrase' do
+    @n = params[:number].to_i
+    @phrase = params[:phrase]
+    string = ""
+    @n.times do
+     string << "#{@phrase}\n"
+    end
+    string
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+  @w1 = params[:word1]
+  @w2 = params[:word2]
+  @w3 = params[:word3]
+  @w4 = params[:word4]
+  @w5 = params[:word5]
+  "#{@w1} " + "#{@w2} " + "#{@w3} "  + "#{@w4} " + "#{@w5}" + "."
+
+  end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+
+    case @operation
+    when "add"
+        "#{@num1 + @num2}"
+      when "subtract"
+        "#{@num1 - @num2}"
+      when "multiply"
+        "#{@num1 * @num2}"
+      when "divide"
+        "#{@num1 / @num2}"
+    end
+  end
 
 end
